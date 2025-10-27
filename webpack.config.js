36be2d8131af3818ce47689dfac412f770ccb045
Wil Linssen <wil@linssen.me>
2025-10-27 22:04:55 +0000

    fix: Update webpack publicPath for GitHub Pages subdirectory
    Change publicPath from '/' to '/fit-file-tools/' to fix 404 errors for bundle.js on GitHub Pages. GitHub Pages serves project sites from a subdirectory based on the repository name.

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.[contenthash].js",
         clean: true,
-        publicPath: "/",
+        publicPath: "/fit-file-tools/",
     },
     module: {
         rules: [
